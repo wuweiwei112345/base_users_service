edac4041753a902d06511fda66a5d97cc1c09f17
wuwei <wudawei112345>
2020-06-24 15:34:55 +0800

    首次提交

---
+++ users-core-service/src/main/java/com/UserApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * @Auther: wuwei
+ * @Date: 2020/6/23 18:16
+ * @Description:程序入口
+ */
+public class UserApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
