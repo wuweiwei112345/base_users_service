8ba95cdc757c64ce6a15a907ff87d6bc7cfd4deb
wuwei <wudawei112345>
2020-06-28 14:48:24 +0800

    添加了 启动 相关 注解 并添加 启动 main 方法语句

--- users-core-service/src/main/java/com/UserApplication.java
+++ users-core-service/src/main/java/com/UserApplication.java
@@ -1,14 +1,29 @@
 package com;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @Auther: wuwei
  * @Date: 2020/6/23 18:16
  * @Description:程序入口
  */
+@Configuration
+@SpringBootApplication(scanBasePackages = {"com.users.*"})
+@MapperScan({"com.users.dao.mapper"})
+@EnableDiscoveryClient
 public class UserApplication {
 
-    public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(UserApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+        log.error("====启动成功====");
     }
 
 }
