59708dbc395a1c01a8b55a4b09643d6ce0df3f64
wuwei <wudawei112345>
2020-06-30 15:16:09 +0800

    添加权限与元素之间的关联数据请求实体 创建

---
+++ users-client/src/main/java/com/users/bean/request/AddPermissionElement.java
@@ -0,0 +1,40 @@
+package com.users.bean.request;
+
+import java.util.Date;
+
+/**
+ * @Auther: wuwei
+ * @Date: 2020/6/30 15:13
+ * @Description:添加权限与元素之间的关联数据请求实体
+ */
+public class AddPermissionElement {
+
+    private Integer permissionId;//权限id
+    private Integer elementId;//权限关联元素id
+    private Integer elementType;//权限关联子元素类型
+
+    public Integer getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(Integer permissionId) {
+        this.permissionId = permissionId;
+    }
+
+    public Integer getElementId() {
+        return elementId;
+    }
+
+    public void setElementId(Integer elementId) {
+        this.elementId = elementId;
+    }
+
+    public Integer getElementType() {
+        return elementType;
+    }
+
+    public void setElementType(Integer elementType) {
+        this.elementType = elementType;
+    }
+
+}
