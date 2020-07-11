9d91adc08ca90934aa63a3e07a4bf270f979c69c
wuwei <wudawei112345>
2020-07-11 16:12:57 +0800

    注册 业务逻辑 登录业务逻辑 提交

--- users-core-service/src/main/java/com/users/service/UsersService.java
+++ users-core-service/src/main/java/com/users/service/UsersService.java
@@ -3,12 +3,21 @@ package com.users.service;
 import com.tools.entity.ResponseEntity;
 import com.tools.mgutil.MD5Utils;
 import com.tools.mgutil.RegUtil;
+import com.users.bean.request.LoginRequestEntity;
 import com.users.bean.request.RegisterUserInfoRequestEntity;
+import com.users.common.RedisLoginTokenCommon;
 import com.users.dao.mapper.UsersMapper;
 import com.users.dao.po.Users;
+import com.users.dao.po.UsersExample;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Auther: wuwei
@@ -20,6 +29,8 @@ public class UsersService {
 
     @Autowired
     private UsersMapper usersMapper;
+    @Autowired
+    private RedisLoginTokenCommon redisLoginTokenCommon;
 
     /**
      * 注册用户信息
@@ -63,6 +74,58 @@ public class UsersService {
             return ResponseEntity.getSuccess(null);
         }else{
             return ResponseEntity.getFail(null);
+        }
+    }
+
+    /**
+     * 用户登录
+     * 功能描述: 用户登录
+     * @param: String userName;//用户名
+     * @param: String userPassword;//用户密码
+     * @return: 统一响应实体 用户登录token
+     * @auther: wuwei
+     * @date: 2020/7/11 15:26
+     */
+    public ResponseEntity login(LoginRequestEntity entity){
+        //参数检查
+        if(entity == null){
+            return ResponseEntity.getFail("请传入参数!");
+        }
+        if(StringUtils.isBlank(entity.getUserName())){
+            return ResponseEntity.getFail("userName参数为必选!");
+        }
+        if(StringUtils.isBlank(entity.getUserPassword())){
+            return ResponseEntity.getFail("userPassword参数为必选!");
+        }
+        if(RegUtil.isAdapterPassword(entity.getUserPassword())){
+            return ResponseEntity.getFail("userPassword参数需合法!");
+        }
+        //查询用户
+        UsersExample example = new UsersExample();
+        example.or().andUserNameEqualTo(entity.getUserName());
+        example.or().andUserEmailEqualTo(entity.getUserName());
+        example.or().andUserPhonenumEqualTo(entity.getUserName());
+        List<Users> list = usersMapper.selectByExample(example);
+        //解析集合
+        if(!CollectionUtils.isEmpty(list)){
+            Users users = list.get(0);
+            //判断密码是否一致
+            if(MD5Utils.getMD5(entity.getUserPassword()).equals(users.getUserPassword())){
+                String userId = users.getUserId().toString();
+                //生成用户登录token
+                String token = redisLoginTokenCommon.createLoginUser(userId);
+                //设置token到redis数据库
+                boolean bool = redisLoginTokenCommon.setLoginToken(userId,token);
+                //组装返回数据
+                Map<String,String> resultMap = new HashMap<String,String>();
+                resultMap.put("userId",userId);
+                resultMap.put("token",token);
+                return ResponseEntity.getSuccessByEntity("登录成功!",resultMap);
+            }else{
+                return ResponseEntity.getFail("用户密码错误!");
+            }
+        }else{
+            return ResponseEntity.getFail("没有此用户!");
         }
     }
 
