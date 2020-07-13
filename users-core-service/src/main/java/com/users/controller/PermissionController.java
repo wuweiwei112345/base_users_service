cb7ccad745e0f5fa520037d9f0fdac570faefac6
wuwei <wudawei112345>
2020-07-13 18:34:53 +0800

    修改了redis ip地址 和 分布式 锁 测试代码

--- users-core-service/src/main/java/com/users/controller/PermissionController.java
+++ users-core-service/src/main/java/com/users/controller/PermissionController.java
@@ -3,10 +3,12 @@ package com.users.controller;
 import com.tools.entity.ResponseEntity;
 import com.users.bean.request.*;
 import com.users.client.PermissionClient;
+import com.users.common.RedisDisLocksCommon;
 import com.users.service.PermissionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -22,11 +24,24 @@ public class PermissionController {
 
     @Autowired
     private PermissionService permissionService;
+    @Autowired
+    private StringRedisTemplate template;
 
     @RequestMapping(value = "/fegintest",method = RequestMethod.POST)
     public ResponseEntity feginTest(@RequestParam(value = "code") Integer code){
-        logger.info("code = " + code);
-        return permissionService.feginTest(code);
+        //实例化锁工具类
+        RedisDisLocksCommon redisDisLocksCommon = new RedisDisLocksCommon(template,String.valueOf(code));
+        //尝试获取锁
+        boolean bool = redisDisLocksCommon.getLock();
+        //准备返回结果
+        ResponseEntity responseEntity = ResponseEntity.getFail("失败!");
+        if(bool){
+            //执行业务逻辑方法
+            responseEntity = permissionService.feginTest(code);
+            //取消锁
+            redisDisLocksCommon.cancelLock();
+        }
+        return responseEntity;
     }
 
     @RequestMapping(value = "/addpermission",method = RequestMethod.POST)
